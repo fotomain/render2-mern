14e28a2619de83df751624677c30aa130132f10f
fotomain <foto888999@gmail.com>
2024-12-10 20:19:59 +0200

    111

--- grapql_schema/schema.js
+++ grapql_schema/schema.js
@@ -32,7 +32,7 @@ const ProjectType = new GraphQLObjectType({
 const ClientType = new GraphQLObjectType({
     name: 'Client',
     fields: () => ({
-        id: { type: GraphQLID },
+        _id: { type: GraphQLID },
         name: { type: GraphQLString },
         email: { type: GraphQLString },
         phone: { type: GraphQLString },
